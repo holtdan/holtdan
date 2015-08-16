ec441e74bcecdda11eac41a8093f5b609621602c
Daniel Holt <HoltDan1@gmail.com>
2015-08-16 14:40:03 -0400

    v1

---
+++ HoltDan/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HoltDan.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            DropboxRestAPI.Client client = new DropboxRestAPI.Client(new DropboxRestAPI.Options
+            {
+                AccessToken = "vBgoZGNyPIMAAAAAAAA0GTo_t9tHdiE_f4EMOGNoelpkTKu7-bWzVNxAN9rdeM4E"
+            });
+            var what = await client.Core.Metadata.MetadataAsync("/Art", include_media_info: true);
+
+            foreach (var c in what.contents)
+            {
+                var x = await client.Core.Metadata.SharesAsync(c.path);
+            }
+            //var token = client.Core.OAuth2.TokenAsync(authCode);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ HoltDan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HoltDan.Startup))]
+namespace HoltDan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
